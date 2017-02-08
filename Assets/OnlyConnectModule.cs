99a54ff22b36720d318c8e69f6c16bd46dd505dd
Timwi <github@timwi.de>
2017-02-08 22:38:55 +0100

    Blank start.

---
+++ Assets/OnlyConnectModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OnlyConnect;
+using UnityEngine;
+using Rnd = UnityEngine.Random;
+
+/// <summary>
+/// On the Subject of Only Connect
+/// Created by Timwi
+/// </summary>
+public class OnlyConnectModule : MonoBehaviour
+{
+    public KMBombInfo Bomb;
+    public KMBombModule Module;
+    public KMAudio Audio;
+
+    void Start()
+    {
+        Debug.Log("[Only Connect] Started");
+    }
+
+    void ActivateModule()
+    {
+        Debug.Log("[Only Connect] Activated");
+    }
+}
